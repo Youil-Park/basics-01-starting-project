c7953a6ceeb1e34b963df872016b45a4519e1235
Youil-Park <parkyouil98@gmail.com>
2024-12-02 14:50:00 +0900

    모든 버튼들을 함수에 연결하기, 코드 재사용

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -5,11 +5,45 @@ function getUserNumberInput() {
     return parseInt(userInput.value);
 }
 
-function add(){
-    const enteredNumber = getUserNumberInput();
-    const calcDescription = `${currentResult} + ${enteredNumber}`;
-    currentResult = currentResult + enteredNumber;
+// 연산
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${currentResult} ${operator} ${calcNumber}`;
     outputResult(currentResult, calcDescription);
 }
 
+// 덧셈
+function add(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult + enteredNumber;
+    createAndWriteOutput('+', initialResult, enteredNumber);
+}
+
+// 뺄셈
+function subtract() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createAndWriteOutput('-', initialResult, enteredNumber);
+}
+
+// 곱하기
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createAndWriteOutput('*', initialResult, enteredNumber);
+}
+
+// 나누기
+function divide () {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createAndWriteOutput('/', initialResult, enteredNumber);
+}
+
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
